feeace93218c4c31c235e8b7d39cd7177f1a3954
Ben Christie <benc00@vt.edu>
2021-01-29 17:13:04 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## Intended use case: mimicking the crowd
+Simply takes in the text file of the zoom chat and outputs the average word of the last 20 lines. Useful for classes.
---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+import sys
+from pyautogui import press, typewrite, hotkey
+import traceback
+from statistics import mode
+import time
+
+def main(pathToTxt, nWords):
+	try:
+		# get all texts
+		f = open(pathToTxt, "r")
+		data = f.readlines()
+		newData = []
+		for line in data[-nWords:]:
+			try:
+				newData.append(line[-line[::-1].index(':')+1:-1])
+			except ValueError:
+				continue
+		# find average word of last n words
+		mostCommon = mode(newData)
+		typewrite(mostCommon)
+		return mostCommon
+
+	except:
+		traceback.print_exc()
+
+
+if __name__ == "__main__":
+	filename="C:/Users/Benjamin/Documents/Zoom/2021-01-22 15.20.24 ME 3034_ Writing as a Mechanical Engineer 94482445948/meeting_saved_chat.txt"
+	time.sleep(5)
+	while True:
+		print('Typewriter wrote : ' + main(filename, 100))
+		time.sleep(30)
+
+else:
+	pass
