6a9fd503d617f0fdeba9febb3a6b53b6896ad6ea
Benjamin Christie <benc00@vt.edu>
2021-02-02 16:30:25 -0500

    added copy-paste method to possible ways to run bot'

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from pyautogui import press, typewrite, hotkey, moveTo, size, position, click
+from pyautogui import press, typewrite, hotkey, moveTo, size, position, click, dragTo
 import traceback
 from statistics import mode
 import time
 from datetime import date
 from collections import Counter
 import os
+import tkinter
+import re
 
 numLines = 100
 delay = 30
@@ -76,26 +78,42 @@ def saveZoomChat(zoomPath, ID, pos=0):
 		return zoomPath + '/' + desiredMeeting[0] + "/meeting_saved_chat.txt"
 
 def semimain():
-	zoomPath = 0
-	zoomMeetingID = ''
-	while type(zoomPath) != str:
-		zoomPath = input("Enter zoom meeting document path (eg C:/Users/Benjamin/Documents/Zoom/): ")
-	while type(zoomMeetingID) != int:
-		try:
-			zoomMeetingID = int(input("Enter zoom meeting ID (should be an 8-13 digit number, eg 92295112497) : "))
-		except ValueError:
-			print("Please enter a 8-13 digit integer")
-	if str(input("Do you want to calibrate your display positions first? Enter [Y/n] : ")).upper() == 'Y':
-		savePos = calibrate()
+	testText = str(input("Do you have save access? (Probably yes) Enter [Y/n] : "))
+	while testText.upper() != "N" and testText.upper() != "Y":
+		testText = str(input("Do you have save access? (Probably yes) Enter [Y/n] : "))
+	if testText.upper() == "N":
+		# does NOT have save access
+		while True:
+			print("Note, this is a lazy approach since file saving is unavailable. Should still work in 99% of cases.")
+			print("Navigate to chat window, fullscreen on primary monitor if uncalibrated, and place your cursor in the chat box")
+			time.sleep(10)
+			text = saveWithoutFile()
+			print('Typewriter wrote : ' + text)
+			print("Waiting {} seconds...".format(str(delay)))
+			time.sleep(delay)
 	else:
-		savePos = 0
-	print("Navigate to chat window, fullscreen on primary monitor if uncalibrated, and place your cursor in the chat box")
-	time.sleep(10)
-	while True:
-		pathToFile = saveZoomChat(zoomPath, zoomMeetingID, savePos)
-		print('Typewriter wrote : ' + main(pathToFile, numLines))
-		print("Waiting {} seconds...".format(str(delay)))
-		time.sleep(delay)
+		zoomPath = 0
+		zoomMeetingID = ''
+		while type(zoomPath) != str:
+			zoomPath = input("Enter zoom meeting document path (eg C:/Users/Benjamin/Documents/Zoom/): ")
+		while type(zoomMeetingID) != int:
+			try:
+				zoomMeetingID = int(input("Enter zoom meeting ID (should be an 8-13 digit number, eg 92295112497) : "))
+			except ValueError:
+				print("Please enter a 8-13 digit integer")
+
+		if str(input("Do you want to calibrate your display save position first? Enter [Y/n] : ")).upper() == 'Y':
+			savePos = calibrate()
+		else:
+			savePos = 0
+		print("Navigate to chat window, fullscreen on primary monitor if uncalibrated, and place your cursor in the chat box")
+		time.sleep(10)
+		while True:
+			pathToFile = saveZoomChat(zoomPath, zoomMeetingID, savePos)
+			print('Typewriter wrote : ' + main(pathToFile, numLines))
+			print("Waiting {} seconds...".format(str(delay)))
+			time.sleep(delay)
+
 
 def calibrate():
 	print("---------------------------------------------------------------\nFOLLOW THESE INSTRUCTIONS CAREFULLY")
@@ -117,8 +135,42 @@ def calibrate():
 	print("Calibrated! Ready to use bot.")
 	return pos
 
+def saveWithoutFile():
+	newData = []
+	TOP_LEFT_POS_RATIO = (24/1920, 35/1080)
+	TOP_RIGHT_POS_RATIO = (1910/1920, 100/1080)
+	BOT_RIGHT_POS_RATIO = (1910/1920, 920/1080)
+	(screenWidth, screenHeight) = size()
+	# get data from screen
+	root = tkinter.Tk()
+	root.withdraw()
+	moveTo(int(screenWidth*TOP_LEFT_POS_RATIO[0]), int(screenHeight*TOP_LEFT_POS_RATIO[1]))
+	dragTo(int(screenWidth/2), screenHeight, 0.2, button='left')
+	hotkey('ctrl', 'c')
+	cpboard = root.clipboard_get()
+	root.destroy()
+	alldata = re.split('\r|\n',cpboard)
+	for line in alldata:
+		if not line.startswith('From') and line != '':
+			newData.append(line)
+	mostCommon = '' # print nothing if broken
+	try:
+		common = Counter(newData).most_common()
+		mostCommon = common[0][0]
+	except:
+		mostcommon = 'broken' # print broken if something broke
+	moveTo(int(screenWidth / 2), screenHeight)
+	click()
+	typewrite(mostCommon + '\n')
+	''' scroll up
+	moveTo(int(screenWidth*BOT_RIGHT_POS_RATIO[0]), int(screenHeight*BOT_RIGHT_POS_RATIO[1]))
+	dragTo(int(screenWidth*TOP_RIGHT_POS_RATIO[0]), int(screenHeight*TOP_RIGHT_POS_RATIO[1]), 0.2, button='left')
+	'''
+	return mostCommon
+
+
+
 if __name__ == "__main__":
 	semimain()
-
 else:
 	pass
